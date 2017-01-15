4b89c4e5b62b273b87a9c292e9971f0c66f70cd5
Trevor Tuchten <tntdirtracer@gmail.com>
2017-01-15 03:08:45 -0800

    checkin : post messages to mongodb

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongo = require('mongodb').MongoClient;
 
 const app = express();
+
+let database;
 
 app.use(bodyParser.json());
 
@@ -14,7 +17,16 @@ app.use((req, res, next) => {
 
 app.post('/api/message', (req, res) => {
 	console.log(req.body); //TODO: Remove Me!!
+	database.collection('messages').insertOne(req.body);
+
 	res.status(200);
+})
+
+mongo.connect("mongodb://localhost:27017/test", (err, db) => {
+	if (!err) {
+		console.log('we are connected to mongo'); //TODO: Remove Me!!
+		database = db;
+	}
 })
 
 let server = app.listen(5000, () => {
